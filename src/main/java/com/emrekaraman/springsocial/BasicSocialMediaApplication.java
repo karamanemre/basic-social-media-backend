3d0d5c4f3d51e8df3129ffc2096b1dff77036665
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-15 00:35:18 +0300

    Initial commit

---
+++ src/main/java/com/emrekaraman/springsocial/BasicSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.emrekaraman.springsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSocialMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emrekaraman/springsocial/BasicSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emrekaraman.springsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
