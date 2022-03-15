279b3e467cc83aaaf47e4585aa69681dfcd294ab
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-16 01:11:54 +0300

    Messages added fields

---
+++ src/main/java/com/emrekaraman/springsocial/core/constants/Messages.java
@@ -0,0 +1,15 @@
+package com.emrekaraman.springsocial.core.constants;
+
+public class Messages {
+
+    public static final String USER_NAME_CAN_NOT_BE_EMPTY = "User name can't be empty";
+    public static final String PASSWORD_CAN_NOT_BE_EMPTY = "Password can't be empty";
+    public static final String FULL_NAME_CAN_NOT_BE_EMPTY = "Full name can't be empty";
+
+    public static final String VALIDATION_EROR_MESSAGE = "Validations errors";
+
+    public static final String PASSWORD_MIN_VALUE = "Password must be min 6 charachter";
+    public static final String PASSWORD_MAX_VALUE = "Password must be max 32 charachter";
+    public static final String PASSWORD_MUST_CONTAIN_THIS = "Password must be,one uppercase, one lowercase, one number and one special charachter contains";
+    public static final String USERNAME_MUST_BE_UNIQE = "Username must be uniqe";
+}
