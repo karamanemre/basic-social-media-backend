b13181a55e81f87c4cbd9d656412e1a4bcff0ef3
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-16 01:12:39 +0300

    Validation exception handler

---
+++ src/main/java/com/emrekaraman/springsocial/core/validationException/ValidationExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.emrekaraman.springsocial.core.validationException;
+
+import com.emrekaraman.springsocial.core.constants.Messages;
+import com.emrekaraman.springsocial.core.utilities.ErrorDataResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice //It allows us to do global exception handling by handling exceptions.
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ValidationExceptionHandler {
+    public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions) {
+        Map<String, String> validationErrors = new HashMap<>();
+        for (FieldError fieldError : exceptions.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        ErrorDataResult<Object> errors = new ErrorDataResult<>(validationErrors, Messages.VALIDATION_EROR_MESSAGE);
+        return errors;
+    }
+}
