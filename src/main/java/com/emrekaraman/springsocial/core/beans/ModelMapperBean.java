b8387485fb200858f0a5d6fee41e4dda92d058b3
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-15 18:11:04 +0300

    Model Mapper bean created

---
+++ src/main/java/com/emrekaraman/springsocial/core/beans/ModelMapperBean.java
@@ -0,0 +1,13 @@
+package com.emrekaraman.springsocial.core.beans;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperBean {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
