b7113a84f5794f9b4861e9e66b1d5a9b3947a618
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-16 01:15:08 +0300

    exclude spring security

--- src/main/java/com/emrekaraman/springsocial/BasicSocialMediaApplication.java
+++ src/main/java/com/emrekaraman/springsocial/BasicSocialMediaApplication.java
@@ -2,8 +2,9 @@ package com.emrekaraman.springsocial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BasicSocialMediaApplication {
 
 	public static void main(String[] args) {
