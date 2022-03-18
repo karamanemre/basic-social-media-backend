8ba5aaa7e25391e7c5e82242a82c58fbdd50ba6d
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-18 15:57:00 +0300

    user auth service added

---
+++ src/main/java/com/emrekaraman/springsocial/auth/userAuthService/UserAuthService.java
@@ -0,0 +1,26 @@
+package com.emrekaraman.springsocial.auth.userAuthService;
+
+import com.emrekaraman.springsocial.business.abstracts.UserService;
+import com.emrekaraman.springsocial.business.constants.Messages;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserAuthService implements UserDetailsService {
+
+    private final UserService userService;
+
+    public UserAuthService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (!userService.existsByUserName(username).isSuccess()){
+            throw new UsernameNotFoundException(Messages.USER_NOT_FOUND);
+        }
+        return new UserDetailsManager(userService.findByUserName(username).getData());
+    }
+}
