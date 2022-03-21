489f50253e658b03b110bc2926686f6219e3589a
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-21 21:06:11 +0300

    "Stream<Object> authorities" field added

--- src/main/java/com/emrekaraman/springsocial/auth/LoginResponse.java
+++ src/main/java/com/emrekaraman/springsocial/auth/LoginResponse.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.stream.Stream;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginResponse {
     private int id;
     private String username;
+    private String password;
+    public  Stream<Object> authorities;
 }
