20e27194df0dcb6c099671a6021a9afce7ef24b9
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-21 21:05:47 +0300

    getAuthority assigned to the field

--- src/main/java/com/emrekaraman/springsocial/auth/AuthManager.java
+++ src/main/java/com/emrekaraman/springsocial/auth/AuthManager.java
@@ -1,17 +1,9 @@
 package com.emrekaraman.springsocial.auth;
 
 import com.emrekaraman.springsocial.auth.userAuthService.UserDetailsManager;
-import com.emrekaraman.springsocial.business.abstracts.UserService;
-import com.emrekaraman.springsocial.business.constants.Messages;
-import com.emrekaraman.springsocial.core.constraint.abstracts.CurrentUser;
 import com.emrekaraman.springsocial.core.utilities.*;
-import com.emrekaraman.springsocial.entities.concretes.User;
 import org.modelmapper.ModelMapper;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class AuthManager implements AuthService {
@@ -23,8 +15,9 @@ public class AuthManager implements AuthService {
     }
 
     @Override
-    public DataResult<LoginResponse> authenticationHandle(UserDetailsManager userDetailsManager) {
+    public DataResult<LoginResponse> authenticationHandle(UserDetailsManager userDetailsManager){
         LoginResponse loginResponse = modelMapper.map(userDetailsManager.getUser(),LoginResponse.class);
+        loginResponse.authorities=userDetailsManager.getAuthorities().stream().map(m->m.getAuthority());
         return new SuccessDataResult(loginResponse);
     }
 
