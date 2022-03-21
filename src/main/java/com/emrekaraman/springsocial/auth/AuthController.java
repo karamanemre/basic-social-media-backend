1c38cf4d2f40129055bbdb2d128fac424d222736
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-21 21:07:19 +0300

    remove unesed import

--- src/main/java/com/emrekaraman/springsocial/auth/AuthController.java
+++ src/main/java/com/emrekaraman/springsocial/auth/AuthController.java
@@ -2,9 +2,7 @@ package com.emrekaraman.springsocial.auth;
 
 import com.emrekaraman.springsocial.auth.userAuthService.UserDetailsManager;
 import com.emrekaraman.springsocial.core.constraint.abstracts.CurrentUser;
-import com.emrekaraman.springsocial.core.utilities.DataResult;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
