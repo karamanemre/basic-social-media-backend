4059bacd33067dcda6e9d583d6ef42a7ebb1f5b4
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-23 01:32:21 +0300

    Pages dto added

---
+++ src/main/java/com/emrekaraman/springsocial/business/dtos/PagesDto.java
@@ -0,0 +1,32 @@
+package com.emrekaraman.springsocial.business.dtos;
+
+import com.emrekaraman.springsocial.entities.concretes.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+@Data
+public class PagesDto<T> {
+
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private int totalPages;
+    private int totalElements;
+    private boolean isFirstPage;
+    private boolean isLastPage;
+
+    public PagesDto(Page<T> page){
+        this.content = page.getContent();
+        this.pageNumber = page.getPageable().getPageNumber();
+        this.pageSize = page.getPageable().getPageSize();
+        this.totalPages = page.getTotalPages();
+        this.totalElements = page.getTotalPages();
+        this.isFirstPage = page.isFirst();
+        this.isLastPage = page.isLast();
+    }
+}
