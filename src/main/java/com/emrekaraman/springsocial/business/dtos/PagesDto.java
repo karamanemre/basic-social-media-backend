abf09e164f2f7dab2812222dae459378b00b7a2d
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-29 12:34:12 +0300

    pages dto revize

--- src/main/java/com/emrekaraman/springsocial/business/dtos/PagesDto.java
+++ src/main/java/com/emrekaraman/springsocial/business/dtos/PagesDto.java
@@ -7,7 +7,9 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class PagesDto<T> {
